003ba797d9ee066403c374118cc13d8a1a3c926a
Mario Suero <mario.suerodejesus@gmail.com>
2025-03-09 15:35:26 -0400

    Creating PUT And DELETE For The CrudAPI

--- Src/RealEase/RealEase.API/Controllers/UserController.cs
+++ Src/RealEase/RealEase.API/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using RealEase.API.Requests;
 using RealEase.API.Responses;
 using RealEase.Domain;
 using RealEase.Domain.Entities;
+using System.Net;
 using static RealEase.API.Requests.NewUserRequest;
 using User = RealEase.Domain.Entities.User;
 
@@ -45,6 +46,43 @@ namespace RealEase.API.Controllers
             //var response = new NewUserResponse { Id = UserDb.Id };
             //return response;
             //return UserDb.Id;
+        }
+
+        [HttpPut("UpdateUser/{id}")]
+        public async Task<ActionResult<NewUserResponse>> UpdateUser(int Id, NewUserRequest request)
+        {
+            var UserDb = await _context.Users.FindAsync(Id);
+
+            if (UserDb == null)
+            {
+                return NotFound();
+            }
+
+            UserDb.Name = request.Name;
+            UserDb.Lastname = request.Lastname;
+            UserDb.IsActive = request.IsActive;
+            UserDb.Address = request.Address;
+
+            _context.Users.Update(UserDb);
+            await _context.SaveChangesAsync();
+
+            return Ok(new NewUserResponse { Id = UserDb.Id });
+
+        }
+
+        [HttpDelete("DeleteUser/{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var UserDb = await _context.Users.FindAsync(id);
+            if (UserDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(UserDb);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
